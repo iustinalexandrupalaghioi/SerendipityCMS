a6aa636bca407f89f1f2ef22674eaf9ce04c3d37
Alex Palaghioi <138356176+iustinalexandrupalaghioi@users.noreply.github.com>
2026-05-17 12:16:18 +0300

    Fix syntax error in datetime formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -72,7 +72,7 @@ export const formatByType = (
     return options?.find((o) => o.value === value)?.label ?? String(value);
   if (type === "date") return format(new Date(value as string), "dd-MM-yyyy");
   if (type === "datetime")
-    return  return formatInTimeZone(parseISO(value), TZ ?? "Europe/Dublin", "dd-MM-yyyy HH:mm")
+    return formatInTimeZone(parseISO(value), TZ ?? "Europe/Dublin", "dd-MM-yyyy HH:mm")
   if (type === "time") return (value as string).slice(0, 5);
   return String(value);
 };
